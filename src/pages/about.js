d7fee57975b2dffbad440e89a5c51fa442ea69a7
NBoris94 <nboris1994@yandex.ru>
2022-11-02 23:12:03 +0300

    Добавил остальные страницы

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Layout from '../components/Layout/Layout'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Новиков Борис - фриланс front-end разработчик и преподаватель</title>
+        <meta name="description" content="" />
+      </Head>
+
+      <Layout>
+
+      </Layout>
+    </>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/cases.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Layout from '../components/Layout/Layout'
+
+const CasesPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Новиков Борис - фриланс front-end разработчик и преподаватель</title>
+        <meta name="description" content="" />
+      </Head>
+
+      <Layout>
+
+      </Layout>
+    </>
+  )
+}
+
+export default CasesPage
